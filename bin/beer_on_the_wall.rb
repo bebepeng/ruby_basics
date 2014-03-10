71d68f775af1dc029a2daf5f27ed9a7a166ebc27
Bebe Peng <bebepeng@gmail.com>
2014-03-09 20:41:12 -0600

    Changed Beer on the Wall to follow ruby conventions

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -10,11 +10,11 @@ if beer == ""
 elsif beer != beer_as_int.to_s
   puts "Error, No number was entered"
 else
-    beer=beer.to_i
+    beer = beer.to_i
 end
 
-#firstBeer is syntax for the first beer number in the song
-def firstBeer(be)
+#first_beer is syntax for the first beer number in the song
+def first_beer(be)
   if be == 1
     "1 bottle"
   else
@@ -22,12 +22,12 @@ def firstBeer(be)
   end
 end
 
-#nextBeer is the syntax for the beer after one has been passed around
-def nextBeer(b)
-  if b==2
+#next_beer is the syntax for the beer after one has been passed around
+def next_beer(b)
+  if b == 2
     "#{b -1} bottle"
   elsif b > 1
-     b-=1
+     b -= 1
     "#{b} bottles"
   else
     "no bottles"
@@ -36,9 +36,9 @@ end
 
 #if a number was inputted, sings the song
 if beer.is_a? Integer
-  while beer > 0 do
-    puts "#{firstBeer(beer)} of beer on the wall, #{firstBeer(beer)} of beer.\nTake one down and pass it around, #{nextBeer(beer)} of beer on the wall.\n"
-    beer-=1
+  while beer > 0
+    puts "#{first_beer(beer)} of beer on the wall, #{first_beer(beer)} of beer.\nTake one down and pass it around, #{next_beer(beer)} of beer on the wall.\n"
+    beer -= 1
   end
 
   if beer == 0
